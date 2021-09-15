28f18f394cab91d3103a220c967d2fa1c3e96ead
Awasuru <pawelzu14@gmail.com>
2021-09-15 10:49:00 +0200

    Review changes: update constructor

--- TASK 1 2ndPack.js
+++ TASK 1 2ndPack.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 class MathEngine{
 	constructor(num1,num2){
     	this.num1=num1;
-      	this.num2=num1;
+      	this.num2=num2;
     }
   	sum(){
     	return this.num1+this.num2;
